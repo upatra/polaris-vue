a40d5c6f3cbf2633f0edea9bf04046eca2648f95
Bao Ho <pippi.blockhead@gmail.com>
2019-10-29 16:59:55 +0700

    feat(component): implement Icon

---
+++ src/components/PIcon/PIcon.vue
@@ -0,0 +1,80 @@
+<template>
+  <span :class="className">
+    <div v-if="source === 'placeholder'" class="Polaris-Icon--Placeholder"/>
+    <img
+      class="Polaris-Icon--Img"
+      :src="source"
+      alt=""
+      aria-hidden="true"
+    />
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { classNames, variationName } from '@/utilities/css';
+
+type Color =
+  | 'white'
+  | 'black'
+  | 'skyLighter'
+  | 'skyLight'
+  | 'sky'
+  | 'skyDark'
+  | 'inkLightest'
+  | 'inkLighter'
+  | 'inkLight'
+  | 'ink'
+  | 'blueLighter'
+  | 'blueLight'
+  | 'blue'
+  | 'blueDark'
+  | 'blueDarker'
+  | 'indigoLighter'
+  | 'indigoLight'
+  | 'indigo'
+  | 'indigoDark'
+  | 'indigoDarker'
+  | 'tealLighter'
+  | 'tealLight'
+  | 'teal'
+  | 'tealDark'
+  | 'tealDarker'
+  | 'greenLighter'
+  | 'green'
+  | 'greenDark'
+  | 'yellowLighter'
+  | 'yellow'
+  | 'yellowDark'
+  | 'orange'
+  | 'redLighter'
+  | 'red'
+  | 'redDark'
+  | 'purple';
+
+const COLORS_WITH_BACKDROPS = [
+  'teal',
+  'tealDark',
+  'greenDark',
+  'redDark',
+  'yellowDark',
+  'ink',
+  'inkLighter',
+];
+
+@Component
+export default class PIcon extends Vue {
+  @Prop(String) public source!: 'placeholder' | string;
+  @Prop(String) public color!: Color;
+  @Prop(Boolean) public backdrop!: boolean;
+
+  public get className() {
+    return classNames(
+      'Polaris-Icon',
+      this.color && `Polaris-Icon--${variationName('color', this.color)}`,
+      this.color && this.color !== 'white' && 'Polaris-Icon--isColored',
+      this.backdrop && 'Polaris-Icon--hasBackdrop',
+    );
+  }
+}
+</script>
