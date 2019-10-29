8916ed56e2f5abb2995312d2d2d117ae28f62c28
Bao Ho <pippi.blockhead@gmail.com>
2019-10-29 09:17:19 +0700

    feat(component): implement Heading

---
+++ src/components/PHeading/PHeading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb;">
+    <component :is="element" :class="className">
+      <slot/>
+    </component>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+type HeadingTagName = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+
+@Component
+export default class PHeading extends Vue {
+  @Prop({ type: String, default: 'h2' }) public element!: HeadingTagName;
+
+  public get className() {
+    return 'Polaris-Heading';
+  }
+}
+</script>
