0c9d8f57e28ae4e90b07f9c2f2804c41c6b74fe2
Bao Ho <pippi.blockhead@gmail.com>
2019-10-29 09:39:08 +0700

    feat(component): implement TextContainer

---
+++ src/components/PTextContainer/PTextContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <p-wrapper>
+    <div :class="className">
+      <slot/>
+    </div>
+  </p-wrapper>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { classNames, variationName } from '@/utilities/css';
+
+import PWrapper from '@/components/PWrapper.vue';
+
+type Spacing = 'tight' | 'loose';
+
+@Component({
+  components: { PWrapper },
+})
+export default class PTextContainer extends Vue {
+  @Prop(String) public spacing!: Spacing;
+
+  public get className() {
+    return classNames(
+      'Polaris-TextContainer',
+      this.spacing && `Polaris-TextContainer--${variationName('spacing', this.spacing)}`,
+    )
+  }
+}
+</script>
