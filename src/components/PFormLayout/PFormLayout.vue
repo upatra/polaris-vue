ec5dcb395d915c7e7529dde2cb57b1283bb273ba
Bao Ho <pippi.blockhead@gmail.com>
2019-10-30 17:31:34 +0700

    feat(component): implement FormLayout
    with the help of @neihc

---
+++ src/components/PFormLayout/PFormLayout.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import PFormLayoutItem from '@/components/PFormLayout/PFormLayoutItem.vue';
+
+@Component({
+  components: { PFormLayoutItem },
+})
+export default class PFormLayout extends Vue {
+  public render(h: any) {
+    const items = (this.$slots.default || []).map((item: any) => h(PFormLayoutItem, { class: { } }, [item]));
+    return h('div', { class: { 'Polaris-FormLayout': true } }, items);
+  }
+}
+</script>
