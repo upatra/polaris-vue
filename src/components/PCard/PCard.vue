df13f3adfc172bbfd014375415ef93a27c4ee9a2
Bao Ho <pippi.blockhead@gmail.com>
2019-10-31 10:38:49 +0700

    feat(component: implement Card

---
+++ src/components/PCard/PCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="className">
+    <PCardHeader v-if="title" :title="title"></PCardHeader>
+    <template v-if="sectioned">
+      <PCardSection>
+        <slot/>
+      </PCardSection>
+    </template>
+    <template v-else>
+      <slot/>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { classNames, variationName } from '@/utilities/css';
+
+import PCardHeader from './PCardHeader.vue';
+import PCardSection from './PCardSection.vue';
+import PCardSubsection from './PCardSubsection.vue';
+
+@Component({
+  components: {
+    PCardHeader, PCardSection,
+  },
+})
+export default class PCard extends Vue {
+  @Prop(String) public title!: string;
+  @Prop(Boolean) public subdued!: boolean;
+  @Prop(Boolean) public sectioned!: boolean;
+
+  public get className() {
+    return classNames(
+      'Polaris-Card',
+      this.subdued && 'Polaris-Card--subdued',
+    );
+  }
+}
+</script>
+
---
+++ src/components/PCard/PCardHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Polaris-Card__Header">
+    <PHeading>{{ title }}</PHeading>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { classNames, variationName } from '@/utilities/css';
+
+import PHeading from '@/components/PHeading/PHeading.vue';
+
+@Component({
+  components: { PHeading },
+})
+export default class PCardHeader extends Vue {
+  @Prop(String) public title!: string;
+}
+</script>
