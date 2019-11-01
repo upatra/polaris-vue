62d03f14c356a75a1124dadcaf3a7f9c17942d5a
Bao Ho <pippi.blockhead@gmail.com>
2019-11-01 18:32:25 +0700

    chore: leap to v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Polaris Vue
+# Polaris Vue by Upatra
+
+Polaris Vue by Upatra is a component library for [Vue.js](https://vuejs.org/) based on [Shopify Polaris style guide](https://polaris.shopify.com/).
+
+## Quick start
+
+You need [Vue.js](https://vuejs.org/) **version 2+**.
+
+### 1. Install via npm
+
+```bash
+npm i @upatra/polaris-vue
+```
+
+### 2. Import and use Polaris Vue
+
+```javascript
+import Vue from 'vue';
+import PolarisVue from '@upatra/polaris-vue';
+import '@upatra/polaris-vue/dist/polaris-vue.css';
+
+Vue.use(PolarisVue);
+```
+
+## Contributing
+
+If you notice any bugs, please create issues. Also, pull requests are welcome.
+
+## License
+
+Code released under [MIT](https://github.com/hgb123/polaris-vue/blob/master/LICENSE) license.
--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,49 @@
-import Vue from "vue";
-import PButton from "./PButton/PButton.vue";
-import PSpinner from "./PSpinner/PSpinner.vue";
+import Vue from 'vue';
+import { PBadge } from './PBadge';
+import { PButton } from './PButton';
+import { PButtonGroup } from './PButtonGroup';
+import { PCard, PCardHeader, PCardSection, PCardSubsection } from './PCard';
+import { PDisplayText } from './PDisplayText';
+import { PFormLayout, PFormLayoutItem } from './PFormLayout';
+import { PHeading } from './PHeading';
+import { PIcon } from './PIcon';
+import { PLayout, PLayoutAnnotatedSection, PLayoutSection } from './PLayout';
+import { PPage } from './PPage';
+import { PSelect } from './PSelect';
+import { PSpinner } from './PSpinner';
+import { PStack, PStackItem } from './PStack';
+import { PSubheading } from './PSubheading';
+import { PTextContainer } from './PTextContainer';
+import { PTextField } from './PTextField';
+import { PTextStyle } from './PTextStyle';
 import '@/scss/main.scss';
 
 const Components = {
+  PBadge,
   PButton,
-  PSpinner
+  PButtonGroup,
+  PCard, PCardHeader, PCardSection, PCardSubsection,
+  PDisplayText,
+  PFormLayout, PFormLayoutItem,
+  PHeading,
+  PIcon,
+  PLayout, PLayoutAnnotatedSection, PLayoutSection,
+  PPage,
+  PSelect,
+  PSpinner,
+  PStack, PStackItem,
+  PSubheading,
+  PTextContainer,
+  PTextField,
+  PTextStyle,
 };
 
-const Polaris = {
+const PolarisVue = {
   install(Vue, options = {}) {
     for (let componentKey in Components) {
       Vue.component(componentKey, Components[componentKey]);
     }
-  }
-}
+  },
+};
 
-export default Polaris
+export default PolarisVue;
