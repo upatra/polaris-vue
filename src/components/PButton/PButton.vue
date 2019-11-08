3722847726fab422e96214cfaf97ee16ca13c28e
Bao Ho <pippi.blockhead@gmail.com>
2019-11-08 10:48:28 +0700

    chore(component): complement icon in Button

--- src/components/PButton/PButton.vue
+++ src/components/PButton/PButton.vue
@@ -15,6 +15,9 @@
           <span class="Polaris-VisuallyHidden">Loading</span>
         </span>
       </span>
+      <span v-if="icon" class="Polaris-Button__Icon">
+        <PIcon :source="loading ? 'placeholder' : icon"/>
+      </span>
       <span class="Polaris-Button__Text">
         <slot/>
       </span>
@@ -26,6 +29,7 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import { classNames, variationName } from '@/utilities/css';
 
+import { PIcon } from '@/components/PIcon';
 import { PSpinner } from '@/components/PSpinner';
 
 type Size = 'slim' | 'medium' | 'large';
@@ -34,7 +38,7 @@ type TextAlign = 'left' | 'right' | 'center';
 const DEFAULT_SIZE = 'medium';
 
 @Component({
-  components: { PSpinner },
+  components: { PIcon, PSpinner },
 })
 export default class PButton extends Vue {
   @Prop(Boolean) public submit!: boolean;
@@ -48,6 +52,7 @@ export default class PButton extends Vue {
   @Prop(Boolean) public fullWidth!: boolean;
   @Prop(String) public size!: Size;
   @Prop(String) public textAlign!: TextAlign;
+  @Prop(String) public icon!: string;
 
   public type: string = '';
 
@@ -66,6 +71,7 @@ export default class PButton extends Vue {
       this.plain && 'Polaris-Button--plain',
       this.monochrome && 'Polaris-Button--monochrome',
       this.fullWidth && 'Polaris-Button--fullWidth',
+      this.icon && this.hasNoChildren && 'Polaris-Button--iconOnly',
       this.size && this.size !== DEFAULT_SIZE && `Polaris-Button--${variationName('size', this.size)}`,
       this.textAlign && `Polaris-Button--${variationName('textAlign', this.textAlign)}`,
     );
@@ -77,6 +83,10 @@ export default class PButton extends Vue {
 
   public get spinnerColor() {
     return this.primary || this.destructive ? 'white' : 'inkLightest';
+  }
+
+  public get hasNoChildren() {
+    return (this.$slots.default || []).length === 0;
   }
 }
 </script>
